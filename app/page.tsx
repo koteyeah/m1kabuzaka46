ea0234fedf51c2285024c5ef3ea54626683570a0
koteyeah <k.kokuta2346@gmail.com>
2024-07-02 14:46:35 +0900

    firstコミット

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,125 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import Loading from "./components/Loading";
+import { nikkei_names, nikkei_tickers } from "./codes";
 
 export default function Home() {
+  const [stockNumber, setStockNumber] = useState("");
+  const [selectedCompany, setSelectedCompany] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [stockData, setStockData] = useState(null);
+  const cardStyle =
+    "p-4 bg-gray-50 shadow-lg rounded-lg transform transition duration-200 hover:scale-105";
+  const handleCompanyChange = (e) => {
+    const selectedName = e.target.value;
+    setSelectedCompany(selectedName);
+
+    const index = nikkei_names.indexOf(selectedName);
+    if (index !== -1) {
+      setStockNumber(nikkei_tickers[index]);
+    } else {
+      setStockNumber("");
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+    setStockData(null);
+
+    try {
+      const response = await fetch(
+        `http://localhost:8000/stock-info/${stockNumber}`
+      );
+
+      if (!response.ok) {
+        setLoading(false);
+        if (response.status === 404) {
+          alert("指定された銘柄の情報が見つかりませんでした。");
+        } else {
+          alert("サーバーエラーが発生しました。");
+        }
+        return;
+      }
+
+      const data = await response.json();
+      setStockData(data);
+      setLoading(false);
+    } catch (error) {
+      console.error("エラーが発生しました:", error);
+      alert("予期しないエラーが発生しました。");
+      setLoading(false);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <img src="Header.png" className="max-w-sm mx-auto" />
+      <main className="flex flex-col items-center min-h-screen">
+        <h1 className="text-4xl p-3 m-3">
+          あなたの入力した株に近い乃木坂の曲をレコメンドします
+        </h1>
+        <form onSubmit={handleSubmit} className="text-center">
+          <select
+            value={selectedCompany}
+            onChange={handleCompanyChange}
+            className="m-4 p-2 border-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-red-200"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            <option value="">会社名を選択してください</option>
+            {nikkei_names.map((name, index) => (
+              <option key={index} value={name}>
+                {name}
+              </option>
+            ))}
+          </select>
+          <button
+            type="submit"
+            className="px-4 py-2 bg-red-400 text-white font-semibold rounded-md shadow hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
+          >
+            検索
+          </button>{" "}
+        </form>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        {loading && <Loading />}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        {stockData && (
+          <div className="flex p-5 border-4 border-slate-200 shadow-lg rounded-lg">
+            {" "}
+            <div className="text-center mx-5">
+              <div className="grid grid-cols-2 gap-4">
+                <p className={cardStyle}>会社名: {stockData["会社名"]}</p>
+                <p className={cardStyle}>業種: {stockData["業種"]}</p>
+                <p className={cardStyle}>
+                  現在の株価: {stockData["current_stock_close"]}
+                </p>
+                <p className={cardStyle}>
+                  過去一ヶ月の平均株価: {stockData["stock_avg_close"]}
+                </p>
+                <p className={cardStyle}>市場価値: {stockData["市場価値"]}</p>
+                <p className={cardStyle}>
+                  市場価値ランキング: {stockData["market_cap_rank"]}
+                </p>
+                <p className={cardStyle}>
+                  値上がり比: {stockData["stock_ratio"]}
+                </p>
+                <p className={cardStyle}>
+                  値上がり比ランキング: {stockData["stock_ratio_rank"]}
+                </p>
+                <p className={cardStyle}>
+                  出来高ランキング: {stockData["trade_ratio_rank"]}
+                </p>
+              </div>
+            </div>
+            <div className={cardStyle + "flex flex-col items-center text-lg"}>
+              <p className="p-3">{stockData["recommend_music"]}</p>
+              <audio controls src={stockData["preview_url"]}>
+                Your browser does not support the audio element.
+              </audio>
+            </div>
+          </div>
+        )}
+      </main>
+    </>
   );
 }
